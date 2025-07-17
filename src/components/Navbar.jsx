55f46ad63d6789c9ff303747f5d6d2d152c99d96
Kailash Kumar <202051093@iiitvadodara.ac.in>
2025-07-18 00:11:18 +0530

    implement the first part of the hero section

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,50 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { navLinks } from "../../constants";
+
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'nav',
+        start: 'bottom top'
+      }
+    });
+
+    navTween.fromTo('nav', {
+      backgroundColor: 'transparent'
+    }, {
+      backgroundColor: "#00000050",
+      backgroundFilter: 'blur(10px)',
+      duration: 1,
+      ease: 'power1.inOut'
+    })
+  });
+
+  return (
+    <nav>
+      <div>
+        <a href="#home" className="flex items-center gap-2">
+          <img
+            src="/images/logo.png"
+            alt="logo"
+            loading="lazy"
+          />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li>
+              <a href={`#${link.id}`} key={link.id}>
+                {link.title}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Navbar
